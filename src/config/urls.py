9fa272ba78899969be1c5b9c3bb2b86ce226ab15
kokishibata <koki@anymindgroup.com>
2019-02-27 15:46:36 +0700

    graphql

--- src/config/urls.py
+++ src/config/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from graphene_django.views import GraphQLView
 
 urlpatterns = [
     path('user/', include("apps.django_user.urls")),
     path('admin/', admin.site.urls),
+    path('graphql/', GraphQLView.as_view(graphiql=True)),
 ]
