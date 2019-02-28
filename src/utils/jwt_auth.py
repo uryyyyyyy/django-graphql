9d95b9a45040a12e30ffc9fc3b5eb087d471f0b9
kokishibata <koki@anymindgroup.com>
2019-02-28 14:15:18 +0700

    mypy

--- src/utils/jwt_auth.py
+++ src/utils/jwt_auth.py
@@ -1,4 +1,6 @@
-from django.contrib.auth import get_user_model
+from typing import Optional
+
+from django.core.exceptions import ObjectDoesNotExist
 from django.core.handlers.wsgi import WSGIRequest
 from django.utils.deprecation import MiddlewareMixin
 
@@ -8,7 +10,7 @@ from datetime import datetime, timedelta
 import jwt
 from jwt import DecodeError
 
-UserModel = get_user_model()
+from apps.for_django.models import User
 
 PRIVATE_KEY = os.environ['JWT_PRIVATE_KEY'].replace("\\n", "\n")
 PUBLIC_KEY = os.environ['JWT_PUBLIC_KEY'].replace("\\n", "\n")
@@ -23,22 +25,22 @@ def encode(user_id: int)-> str:
     ).decode("utf-8")
 
 
-def decode(token: str)-> int:
+def decode(token: str)-> Optional[int]:
     try:
         return jwt.decode(str.encode(token), PUBLIC_KEY, algorithms='RS256')['user_id']
     except DecodeError:
         return None
 
 
-def get_user(user_id):
+def get_user(user_id) -> Optional[User]:
     try:
-        return UserModel._default_manager.get(pk=user_id)
-    except UserModel.DoesNotExist:
+        return User.objects.get(pk=user_id)
+    except ObjectDoesNotExist:
         return None
 
 
 class JWTAuthenticationMiddleware(MiddlewareMixin):
-    def process_request(self, request: WSGIRequest):
+    def process_request(self, request):
         token = request.META.get('HTTP_AUTHORIZATION')
         if token is not None:
             user_id = decode(token)
--- src/utils/oauth/facebook.py
+++ src/utils/oauth/facebook.py
@@ -1,4 +1,5 @@
 import os
+from typing import Optional
 
 import requests
 from requests import Response
@@ -8,7 +9,7 @@ OAUTH2_SECRET = os.environ['OAUTH2_FACEBOOK_SECRET']
 OAUTH2_REDIRECT = os.environ['OAUTH2_FACEBOOK_REDIRECT']
 
 
-def oauth(code: str)-> dict:
+def oauth(code: str)-> Optional[dict]:
     url = f'https://graph.facebook.com/v3.2/oauth/access_token?client_id={OAUTH2_ID}' \
         f'&redirect_uri={OAUTH2_REDIRECT}&client_secret={OAUTH2_SECRET}&code={code}'
     response: Response = requests.get(url)
@@ -18,7 +19,7 @@ def oauth(code: str)-> dict:
     return response.json()
 
 
-def get_user_detail(token: str) -> dict:
+def get_user_detail(token: str) -> Optional[dict]:
     url = f'https://graph.facebook.com/v3.2/me?access_token={token}&fields=id,email'
     response: Response = requests.get(url)
     if response.status_code != 200:
